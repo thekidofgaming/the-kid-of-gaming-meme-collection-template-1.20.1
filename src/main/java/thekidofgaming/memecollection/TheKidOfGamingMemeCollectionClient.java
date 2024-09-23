a1eaa704e9abc7b52144d5e372cc2748de4eb959
thekidofgaming <alanproc.ap@hotmail.com>
2024-09-23 18:46:19 +0100

    ystrh

---
+++ src/main/java/thekidofgaming/memecollection/TheKidOfGamingMemeCollectionClient.java
@@ -0,0 +1,10 @@
+package thekidofgaming.memecollection;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TheKidOfGamingMemeCollectionClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/thekidofgaming/memecollection/TheKidOfGamingMemeCollectionDataGenerator.java
@@ -0,0 +1,11 @@
+package thekidofgaming.memecollection;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TheKidOfGamingMemeCollectionDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
